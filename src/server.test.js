e7c0817ec9fa483ff6be439f7e8349b546d1be8a
abitravers1989 <atravers4007@gmail.com>
2018-12-03 17:58:48 +0000

    boilerPlate for first set of server tests

---
+++ src/server.test.js
@@ -0,0 +1,23 @@
+const serverFactory = require('./server');
+
+describe('Server', () => {
+    describe('start', () => {
+        it('starts up the express server', () => {
+
+        });
+        it('emits a listened event', () => {
+
+        });
+        it('prints to the console when it hears that event', () => {
+
+        });
+        describe('throws an error if something went wrong', () => {
+            it('loggs the error', () => {
+
+            })
+            it('shuts down gracefully', () => {
+                
+            })
+        })
+    })
+})
