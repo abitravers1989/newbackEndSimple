1ac2e0693be5afa30145405805bbf3c54c882b03
abitravers1989 <atravers4007@gmail.com>
2018-12-07 17:38:24 +0000

    add logging to server

--- src/server.js
+++ src/server.js
@@ -1,20 +1,45 @@
-module.exports = ({ app, envVariables, logger }) => {
+module.exports = ({ app, envVariables, morgan, logger }) => {
+    let server;
     return {
         start: () => {
             try {
-                app.listen(envVariables.PORT, () => {
-                    app.emit('listened', null)
-                }) 
-                
-                app.on('listened', () => {
-                    console.log(`listening on port ${envVariables.PORT}`)
-                    logger.info(`listening on port ${envVariables.PORT}`)
-                })  
+                app.use(morgan('dev', {
+                    skip: (req, res) => {
+                        return res.statusCode < 400
+                    }, stream: process.stderr
+                }));
+
+                app.use(morgan('dev', {
+                    skip: (req, res) => {
+                        return res.statusCode >= 400
+                    }, stream: process.stdout
+                }));
+
+                server = app.listen(envVariables.PORT, () => {
+                    logger.info(`listening on port ${server.address().port}`)
+                    //console.log((`listening on port ${server.address().port}`))
+                }); 
             } catch (err) {
                 logger.error(err, 'Failed to start server')
-                process.exit()
+                //process.exit(1)
             }
+            return server;
         }
     }
 }
+
+
+//HAVING PROBLEMS WITHY THE LOGGER
+
+// try {
+//     functionWithACallback(objectParameter, callbackFunction() => {
+//         checkTheValidityOfObjectParameter
+//         callbackFunction();
+//     })
+// } catch (err) {
+//     logger.fatal(err, 'Error while executing callbackFunction');
+//     process.exit(1);
+// }
+
+// expect(() => invokeFunctionWhichWillThrowAnErrorHERE()).to.throw(TypeError)
 
