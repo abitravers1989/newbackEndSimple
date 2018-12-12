cc964fee1df8affb9dd0195d53da62518655c48f
abitravers1989 <atravers4007@gmail.com>
2018-12-12 11:34:25 +0000

    test for start functionality working

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ module.exports = ({ app, envVariables, morgan, logger }) => {
     return {
         start: () => {
             try {
-                
                 app.use(morgan('dev', {
                     skip: (req, res) => {
                         return res.statusCode < 400
@@ -15,7 +14,6 @@ module.exports = ({ app, envVariables, morgan, logger }) => {
                         return res.statusCode >= 400
                     }, stream: process.stdout
                 }));
-                console.log('coming in here?')
                 server = app.listen(envVariables.PORT, () => {
                     logger.info(`listening on port ${server.address().port}`)
                     //console.log((`listening on port ${server.address().port}`))
@@ -24,7 +22,7 @@ module.exports = ({ app, envVariables, morgan, logger }) => {
                 logger.fatal(err, 'Failed to start server')
                 process.exit(1)
             }
-            return server, app;
+            return server;
         }
     }
 }
@@ -32,15 +30,4 @@ module.exports = ({ app, envVariables, morgan, logger }) => {
 
 //HAVING PROBLEMS WITHY THE LOGGER
 
-// try {
-//     functionWithACallback(objectParameter, callbackFunction() => {
-//         checkTheValidityOfObjectParameter
-//         callbackFunction();
-//     })
-// } catch (err) {
-//     logger.fatal(err, 'Error while executing callbackFunction');
-//     process.exit(1);
-// }
-
-// expect(invokeFunctionWhichWillThrowAnErrorHERE).to.throw(TypeError)
 
--- src/server.test.js
+++ src/server.test.js
@@ -2,58 +2,62 @@ const serverFactory = require('./server');
 const sinon = require('sinon')
 
 describe('Server', () => {
-    const sandbox = sinon.createSandbox();
-    const dependencies = {
-        app: {
-            listen: sandbox.stub(),
-            use: () => {},
-        },
-        envVariables: {
-            PORT: 3000,
-            NODE_ENV: 'dev',
-        },
-        morgan: {
-            dev: ()=>{}
-        },
-        logger: {
-            fatal: () => {}
-        }
-    }
+  const sandbox = sinon.createSandbox();
+  const dependencies = {
+    app: {
+      listen: sandbox.stub(),
+      use: sandbox.stub(),
+     },
+     envVariables: {
+       PORT: 3000,
+       NODE_ENV: 'dev',
+     },
+     morgan: ()=>{},
+     logger: {
+       info: () => {},
+       fatal: () => {},
+     },
+    };
 
-    const server = serverFactory(dependencies);
+  const server = serverFactory(dependencies);
 
-    //const { app } = dependencies;
+  const { app } = dependencies;
 
-    beforeEach(() => {
-        sandbox.stub(process, 'exit')
-    })
+  beforeEach(() => {
+    sandbox.stub(process, 'exit')
+  })
+  afterEach(() => sandbox.reset());
 
-    afterEach(() => sandbox.reset());
+  describe('start', () => {
+    fit('starts up the express server', () => {
+      const mockExpress = {
+        address: () => ({
+          PORT: dependencies.envVariables.PORT,
+        }),
+      };
+      app.listen.returns(mockExpress);
+      server.start();
+      app.listen.yield();
 
-    //afterAll(() => sandbox.restore());
-
-    describe('start', () => {
-        fit('starts up the express server', () => {
-            
-            const actualServer= server.start();
-            console.log('actualServer', actualServer)
+      const actualServer= server.start();
+      console.log('actualServer', actualServer)
 
             // console.log("app is!!!!", dependencies.app)
             //expect(dependencies.app.listen).to.have.been.called
             //With(dependencies.envVariables.PORT);
-        });
+     });
  
-        it('Logs the port which the server has been started on', () => {
+     it('Logs the port which the server has been started on', () => {
 
-        });
-        describe('when the server creation fails', () => {    
-            it('shuts down the server', () => {
-                app.listen.throws();
-                server.start()
+     });
+     describe('when the server creation fails', () => {    
+       it('shuts down the server', () => {
+         app.listen.throws();
+         server.start()
                 
                 // have to mock it out 
                 //expect(process.exit).to.have.been.calledWith(1)
-            })
         })
-    })
+     })
+  })
 })
