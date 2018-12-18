da6fd65f3f31a1ea93932e8a23970b46bc13f2fe
abitravers1989 <atravers4007@gmail.com>
2018-12-18 15:03:09 +0000

    add and test ability to add a default value to the getEnvVar file

--- src/getEnvVar.js
+++ src/getEnvVar.js
@@ -1,5 +1,6 @@
 module.exports = (name, defaultVal) => {
     if (
+        defaultVal === undefined &&
         (process.env[name] === undefined || process.env[name] === '')
     ) {
         throw new ReferenceError(`Environment variable ${name} is required`)
--- src/getEnvVar.test.js
+++ src/getEnvVar.test.js
@@ -6,13 +6,49 @@ chai.use(require('sinon-chai'));
 
 const getEnvVar = require('./getEnvVar');
 
-describe('getEnvVar', () => {
-    describe.only('when the requested env variable is found', () => {
+describe.only('getEnvVar', () => {
+    describe('when the requested env variable is found', () => {
         it('returns the value', () => {
             const MOCK_ENV_VARIABLE = 'MOCK_ENV_VARIABLE';
             const mockVariableValue = 'test';
             process.env[MOCK_ENV_VARIABLE] = mockVariableValue;
             expect(getEnvVar('MOCK_ENV_VARIABLE')).to.equal(mockVariableValue)
         });
+    });
+
+    describe('when the requested env variable has an invalid value', () => {
+        describe('when the value is undefined', () => {
+            it('throws an error', () => {
+                const nonExistenetVariable = 'undefined';
+                expect(() => {
+                    getEnvVar(nonExistenetVariable);
+                  }).to.throw(
+                    ReferenceError,
+                    `Environment variable ${nonExistenetVariable} is required`,
+                  );
+            });
+        });
+
+        describe('when the value is an empty string', () => {
+            it('throws an error', () => {
+                const MOCK_ENV_VARIABLE = 'MOCK_ENV_VARIABLE';
+                process.env[MOCK_ENV_VARIABLE] = '';
+                expect(() => {
+                    getEnvVar(MOCK_ENV_VARIABLE);
+                  }).to.throw(
+                    ReferenceError,
+                    `Environment variable ${MOCK_ENV_VARIABLE} is required`,
+                  );
+            });
+        });
+
+        describe('a default value is provided also', () => {
+            it('returns the value', () => {
+                const MOCK_ENV_VARIABLE = 'MOCK_ENV_VARIABLE';
+                process.env[MOCK_ENV_VARIABLE] = '';
+                const defaultValue = 'default';
+                expect(getEnvVar(MOCK_ENV_VARIABLE, defaultValue)).to.equal(defaultValue);
+            });
+        })
     });
 })
