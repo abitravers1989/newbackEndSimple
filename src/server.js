45129c5048562aa5fbcfcd3d2f60e04fee6d8343
abitravers1989 <atravers4007@gmail.com>
2018-12-21 18:42:19 +0000

    connect to database to prove working

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-module.exports = ({ app, envVariables, morgan, logger, routes, bodyParser, mongoose, promisify, cors, helmet,  articleEndpoint }) => {
+module.exports = ({ app, envVariables, morgan, logger, routes, bodyParser, promisify, cors, helmet,  articleEndpoint }) => {
     let server;
     return {
         start: () => {
-            process.on('uncaughtException', (err) => {
-                console.error('Unhandled Exception', err)
-              })
+            // process.on('uncaughtException', (err) => {
+            //     console.error('Unhandled Exception', err)
+            //   })
               
-              process.on('uncaughtRejection', (err, promise) => {
-                console.error('Unhandled Rejection', err)
-              })
+            //   process.on('uncaughtRejection', (err, promise) => {
+            //     console.error('Unhandled Rejection', err)
+            //   })
 
             try {
                 app.use(morgan('dev', {
@@ -38,27 +38,47 @@ module.exports = ({ app, envVariables, morgan, logger, routes, bodyParser, mongo
 
                 app.use((err, req, res, next) => {
                     new Error('An error occured in the app: ', err);
-                    res.status(500).send('There was an issue in the app')
+                    res.status(500).send('There was an issue in the app:', err)
                 });
 
-                app.post('/api', (req, res) => {
-                    console.log(req)
-                    res.render('request is:  ', req)
-                })
-//!!!!!!!!!!! reafctor this into a repository file with a start method and a ctahc for the error
-                //add database
-                mongoose.connect('mongodb://localhost/blogSite', { useNewUrlParser: true });
-                mongoose.Promise = global.Promise;
-                mongoose.set('debug', true);
-                // Get the default connection
-                const db = mongoose.connection;
-                //bind the connection to an error event to get notifications of connection errors 
-                db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-//!!!!!!!!!!!!!!
+//                 const mongoose = require('mongoose');
+// //!!!!!!!!!!! reafctor this into a repository file with a start method and a ctahc for the error
+//                 //add database
+//                 mongoose.connect('mongodb://localhost:27017/blogSite', { useNewUrlParser: true });
+//                 mongoose.Promise = global.Promise;
+//                 mongoose.set('debug', true);
+//                 // Get the default connection
+//                 const db = mongoose.connection;
+//                 //bind the connection to an error event to get notifications of connection errors 
+//                 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+              
+//                 var postSchema = new mongoose.Schema({body: String});
+//                 var Post = mongoose.model('Post', postSchema);
 
-                //!!!!!!!!!
-                //add model
-                //req(model/article)
+//                 var postData = new Post({body: 'Stringbbhhh'});
+//                 postData.save().then(result => {
+//                     console.log('saved')
+//                 }).catch(err => {
+//                     console.log('unable to save')
+//                 });
+
+//                 Post.find({}, (err, posts) => {
+//                     console.log(posts)
+//                 })
+                 
+
+//!!!!!!!!!!!!!!
+                
+                var mongo = require('mongodb');
+                var monk = require('monk');
+                var db = monk('localhost:27017/blogSite')
+
+                var collection = db.get('posts');
+
+                collection.find({}, {}, function (err, docs) {
+                    console.log(docs)
+                })
+
 
                 //startup the server
                 server = app.listen(envVariables.PORT, () => {
