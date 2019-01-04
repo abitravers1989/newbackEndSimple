c1c4d632b21cbfa5e9973b2de941387c8462cdc2
abitravers1989 <atravers4007@gmail.com>
2019-01-04 17:08:42 +0000

    add better error handling on get route

--- src/rest/routes/articles.js
+++ src/rest/routes/articles.js
@@ -1,37 +1,32 @@
 
 module.exports = ({ mongoose, articlevalidation}) => { 
   return {
-    create: (req, res) => {
-     const { body } = req;  
-      articlevalidation.isvaid(body);
 
-      // try {
-        const { title, articleBody, author } = body;
-        const Article = mongoose.model('Article'); 
-        var articleData = new Article({ title, articleBody, author});
-        articleData.save().then(result => {
-            console.log('saved')
-         }).catch(err => {
-             console.log('unable to save')
-         }); 
-      // } catch(err) {
-      //   //replace with logger
-      //   console.log(`An error occured while trying to save the article to the db:`, err)
-      // }
+    create: (req, res) => {
+      const { body } = req;  
+      articlevalidation.isvaid(body);
+      const { title, articleBody, author } = body;
+      const Article = mongoose.model('Article'); 
+      const articleData = new Article({ title, articleBody, author});
+      articleData.save().then(result => {
+        console.log('saved')
+      }).catch(err => {
+        console.log('unable to save')
+      }); 
       res.status(200).json({ status: 'successfully posted:', title, articleBody, author });
     },
+
     get: (req, res, next) => {
-      // try {
-        const Article = mongoose.model('Article');  
-        return Article.find()
-          .sort({ createdAt: 'descending' })
-          .then((articles) => res.json({articles: articles.map(article => article.toJSON()) }))
-          .catch(next);
-      // } catch(err) {
-      //   //replace with logger
-      //   console.log(`An error occured while trying to get all articles from the db:`, err)
-      // }    
+      const Article = mongoose.model('Article');  
+      return Article.find()
+      .sort({ createdAt: 'descending' })
+      .then((articles) => 
+        res.json({articles: articles.map(article => article.toJSON()) 
+      })).catch(err => {
+        console.log('unable to get all articles because', err)
+      });   
     },
+
     getById: (req, res) => {
      // const Article = mongoose.model('Article');
      console.log('params are', req.query.title)
@@ -41,13 +36,15 @@ module.exports = ({ mongoose, articlevalidation}) => {
       //   res.status(200).json(article)
       // })
     },
+
     deleteArticle: (req, res) => { 
       const Article = mongoose.model('Article'); 
+      //TODO validate query.id
       return Article.findByIdAndRemove(req.query.id)
       .then(result => {
-        res.status(200).json({status: `successfully deleted Article with id: ${req.query.id}` })
+        res.status(200).json({status: `successfully deleted Article with given id: ${req.query.id}` })
       }).catch(err => {
-        console.log(`unable to delete: ${req.query.id}`, err)
+        console.log(`unable to deleteArticle with provided ID`, err)
       });
     },
   }
