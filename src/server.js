5045ed7a85c82bd5efabf31db1cdef5bddc793e0
abitravers1989 <atravers4007@gmail.com>
2019-02-21 13:59:16 +0000

    linting

--- src/server.js
+++ src/server.js
@@ -1,55 +1,67 @@
-module.exports = ({ app, envVariables, morgan, logger, routes, bodyParser, promisify, mongodb, articleSchema }) => {
-    let server;
-    return {
-        start: () => {
-            try {
-                app.use(morgan('dev', {
-                    skip: (req, res) => {
-                        return res.statusCode < 400
-                    }, stream: process.stderr
-                }));
+module.exports = ({
+  app,
+  envVariables,
+  morgan,
+  logger,
+  routes,
+  bodyParser,
+  promisify,
+  mongodb,
+  articleSchema
+}) => {
+  let server
+  return {
+    start: () => {
+      try {
+        app.use(
+          morgan('dev', {
+            skip: (req, res) => {
+              return res.statusCode < 400
+            },
+            stream: process.stderr
+          })
+        )
 
-                app.use(morgan('dev', {
-                    skip: (req, res) => {
-                        return res.statusCode >= 400
-                    }, stream: process.stdout
-                }));
+        app.use(
+          morgan('dev', {
+            skip: (req, res) => {
+              return res.statusCode >= 400
+            },
+            stream: process.stdout
+          })
+        )
 
-                app.use(bodyParser.json());
-                app.use(bodyParser.urlencoded({ extended: true }));
-               // app.use(cors());
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({ extended: true }))
+        // app.use(cors());
 
-                //add routes 
-                routes.setupEndpoints(app);
-                
-                //connect to the database
-                mongodb.connect();
+        // add routes
+        routes.setupEndpoints(app)
 
-                //create the schema 
-                articleSchema.createArticleSchema();
+        // connect to the database
+        mongodb.connect()
 
-                //startup the server
-                server = app.listen(envVariables.PORT, () => {
-                    logger.info(`listening on port ${server.address().port}`)
-                }); 
+        // create the schema
+        articleSchema.createArticleSchema()
 
-            } catch (err) {
-                logger.error(err, 'Failed to start server');
-                process.exit(1)
-            }
-            return server;
-        },
-        stop: async () => {
-            try {
-                await promisify(server.close).call(server);
-                logger.info('Shutting down the server successfully')
-            } catch (err) {
-                logger.error({err}, 'Forcing server to shut down');
-                process.exit(1);
-            };
-        },
-    };
-};
-
-
-
+        // startup the server
+        server = app.listen(envVariables.PORT, () => {
+          logger.info(`listening on port ${server.address().port}`)
+        })
+      } catch (err) {
+        logger.error(err, 'Failed to start server')
+        process.exit(1)
+      }
+      return server
+    },
+    stop: async () => {
+      try {
+        await promisify(server.close).call(server)
+        logger.info('Shutting down the server successfully')
+      } catch (err) {
+        logger.error({ err }, 'Forcing server to shut down')
+        process.exit(1)
+      }
+    }
+  }
+}
